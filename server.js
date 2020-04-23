1433f781b4daa5cfa5b6ed148f5552e4836823b1
Catie Tortorella <catietortorella@gmail.com>
2020-04-23 16:13:44 -0700

    added sass to app

--- server.js
+++ server.js
@@ -7,9 +7,10 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}))
+app.use(express.static(path.join(__dirname, 'css')));
 
 app.use('/admin', adminRoutes)
-app.use('/shop', shopRoutes)
+app.use(shopRoutes)
 
 app.use((req,res,next) => {
     res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
